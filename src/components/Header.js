4f7bccd5e69aeea3fc3e56d3cfadf901b6f0cee1
mainangaruiya <003paulmaina@gmail.com>
2025-01-15 15:44:07 +0300

    fitness app here we go

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+// src/components/Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="bg-orange-600 text-white py-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Fitness App</h1>
+        <ul className="flex space-x-6">
+          <li>
+            <Link to="/" className="hover:text-gray-200">Home</Link>
+          </li>
+          <li>
+            <Link to="/signup" className="hover:text-gray-200">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login" className="hover:text-gray-200">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
