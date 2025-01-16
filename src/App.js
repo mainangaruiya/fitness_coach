b0de74038030ae357897e5a22bae5e23c4331360
mainangaruiya <003paulmaina@gmail.com>
2025-01-16 13:57:30 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/pages/HomePage';
+import HomePage from './pages/HomePage';
 
 
 function App() {
